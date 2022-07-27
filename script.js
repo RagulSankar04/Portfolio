2c3013295a66dd2717040dfa3ffcd63dd8edc667
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-27 19:38:20 +0530

    ✅ Typed Js ✅

--- script.js
+++ script.js
@@ -9,8 +9,23 @@ window.onscroll = () => {
   menu.classList.remove("fa-times");
   header.classList.remove("active");
 };
-var typed = new Typed(".typing-text", {
-  strings: ["Web Developer", "Web Designer", "Web Designer & Developer"],
-  loop: true,
-  typeSpeed: 150,
-});
+
+const typed = document.querySelector(".typed");
+
+if (typed) {
+  let typed_strings = typed.getAttribute("data-typed-items");
+
+  typed_strings = typed_strings.split(",");
+
+  new Typed(".typed", {
+    strings: typed_strings,
+
+    loop: true,
+
+    typeSpeed: 100,
+
+    backSpeed: 50,
+
+    backDelay: 2000,
+  });
+}
