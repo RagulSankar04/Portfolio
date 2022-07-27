5f17ac083f91db30f5e7b362df0142213f00b29d
Ragul <ragulsankar2004@gmail.com>
2022-07-27 18:37:30 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+var typed= new Typed('typing-text', {
+    strings: ["Web Developer", "Web Designer", "Web Designer & Developer"],
+    loop: true,
+    typespeed: 150,
+})
