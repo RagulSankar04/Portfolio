7dc29617a7acf740824832170b9eb1deaec317ba
Ragul <ragulsankar2004@gmail.com>
2022-07-27 19:24:17 +0530

    changed Directories

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
-var typed= new Typed('typing-text', {
+var typed= new Typed('.typing-text', {
     strings: ["Web Developer", "Web Designer", "Web Designer & Developer"],
     loop: true,
-    typespeed: 150,
+    typespeed: 0,
 })
