5775d1a6f14b2d4c16a603263803cf8af9160733
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-27 19:25:33 +0530

    Typed Js Fixed ✅

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+let menu = document.querySelector("#menu-bars");
+let header = document.querySelector("header");
 
-let menu = document.querySelector('#menu-bars');
-let header = document.querySelector('header');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-}
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    header.classList.remove('active');
-}
-var typed= new Typed('.typing-text', {
-    strings: ["Web Developer", "Web Designer", "Web Designer & Developer"],
-    loop: true,
-    typespeed: 0,
-})
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  header.classList.toggle("active");
+};
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  header.classList.remove("active");
+};
+var typed = new Typed(".typing-text", {
+  strings: ["Web Developer", "Web Designer", "Web Designer & Developer"],
+  loop: true,
+  typeSpeed: 150,
+});
